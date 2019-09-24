0019800dfb0e9915d815bbfc89bc895a3d16e908
tracer-ics2019 <tracer@njuics.org>
2019-09-24 19:31:24 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-62-generic #69-Ubuntu SMP Wed Sep 4 20:55:53 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     19:31:24 up  4:28,  1 user,  load average: 2.16, 1.68, 1.62
    c0939417e853ee01597bf02d988b0a3680e3fd90

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -9,19 +9,21 @@ void ui_mainloop(int);
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
-/*
-  bool *success = (bool*)malloc(sizeof(bool));
-  FILE *f = fopen("./tools/gen-expr/input", "r");
-  char exp[10000];
-  int correct;
-  for (int i=0; i<10; ++i){
-    exp[0] = '\0';
-    if (fscanf(f, "%u %s", &correct, exp)!=1) continue;
-    int result = expr(exp, success);
-    if (*success==true) printf("%u %u", correct, result);
-  }
-  free(success);
-*/
+
+  /*
+    bool *success = (bool*)malloc(sizeof(bool));
+    FILE *f = fopen("./tools/gen-expr/input", "r");
+    char exp[10000];
+    int correct;
+    for (int i=0; i<10; ++i){
+      exp[0] = '\0';
+      if (fscanf(f, "%u %s", &correct, exp)!=1) continue;
+      int result = expr(exp, success);
+      if (*success==true) printf("%u %u", correct, result);
+    }
+    free(success);
+  */
+ 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
