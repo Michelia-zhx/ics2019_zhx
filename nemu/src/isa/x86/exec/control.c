5f139433595656207d3182e30168dbd11a57cbe6
tracer-ics2019 <tracer@njuics.org>
2019-10-12 17:45:20 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     17:45:20 up 18:34,  1 user,  load average: 2.10, 1.96, 1.86
    ddb72123a45cb42274e4a70852b50cfd5494dce8

--- nemu/src/isa/x86/exec/control.c
+++ nemu/src/isa/x86/exec/control.c
@@ -28,8 +28,7 @@ make_EHelper(call) {
   // the target address is calculated at the decode stage
   rtl_li(&s1, decinfo.seq_pc);
   rtl_push(&s1);
-  decinfo.jmp_pc = 1;
-
+  decinfo.is_jmp = 1;
   print_asm("call %x", decinfo.jmp_pc);
 }
 
