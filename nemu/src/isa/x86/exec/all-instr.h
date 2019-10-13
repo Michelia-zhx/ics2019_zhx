55eed4d99d756bf66fd6cda08f3cc9f85955e132
tracer-ics2019 <tracer@njuics.org>
2019-10-13 10:02:25 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     10:02:25 up 10:55,  1 user,  load average: 2.36, 1.81, 1.39
    37e78e7110e7cd08895ce3ace1a6019fe1a3c7e0

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -1,8 +1,9 @@
 #include "cpu/exec.h"
 
 make_EHelper(mov);
-make_EHelper(call);
+make_EHelper(push);
 make_EHelper(operand_size);
 
+make_EHelper(call);
 make_EHelper(inv);
 make_EHelper(nemu_trap);
