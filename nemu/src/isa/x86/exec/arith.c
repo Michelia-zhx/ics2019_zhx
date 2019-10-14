be23324d19119499b93cd1ca87347eeb2b56714b
tracer-ics2019 <tracer@njuics.org>
2019-10-14 21:10:03 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     21:10:03 up 1 day, 22:02,  1 user,  load average: 1.57, 1.77, 1.71
    2234b70fd52b5bfcc93e6ab4bc2a6ee53bcaa31b

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -9,27 +9,20 @@ make_EHelper(add) {
 make_EHelper(sub) {
   // s0 = dest - src
   rtl_sub(&s0, &id_dest->val, &id_src->val);
-  // s1 = s0 - CF
-  rtl_get_CF(&s1);
-  rtl_sub(&s1, &s0, &s1);
+  rtl_setrelop(RELOP_LTU, &s1, &id_dest->val, &s0);
 
-  operand_write(id_dest, &s1);
+  operand_write(id_dest, &s0);
 
-  if (id_dest->width != 4) {
-    rtl_andi(&s1, &s1, 0xffffffffu >> ((4 - id_dest->width) * 8));
-  }
-
-  rtl_update_ZFSF(&s1, id_dest->width);
-
-  // update CF
-  rtl_is_sub_carry(&s1, &s1, &s0);
-  rtl_is_sub_carry(&s0, &s0, &id_dest->val);
-  rtl_or(&s0, &s0, &s1);
-  rtl_set_CF(&s0);
-
-  // update OF
-  rtl_is_sub_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);
-  rtl_set_OF(&s0);
+  rtl_update_ZFSF(&s0, id_dest->width);
+  rtl_setrelop(RELOP_LTU, &t0, &id_dest->val, &s0);
+  rtl_or(&t0, &s1, &t0);
+  rtl_set_CF(&t0);
+  
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &s0);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template2(sub);
 }
