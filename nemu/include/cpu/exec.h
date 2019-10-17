d1c19b382ead284f7b532ddeaca6220e4532376e
tracer-ics2019 <tracer@njuics.org>
2019-10-17 15:49:09 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     15:49:09 up 16:37,  1 user,  load average: 2.32, 1.91, 1.72
    a0c6e5f65765f6a6b890e7baa312c3781a38c93

--- nemu/include/cpu/exec.h
+++ nemu/include/cpu/exec.h
@@ -38,6 +38,7 @@ static inline uint32_t instr_fetch(vaddr_t *pc, int len) {
 
 /* Instruction Decode and EXecute */
 static inline void idex(vaddr_t *pc, OpcodeEntry *e) {
+  printf("%ls\n", pc);
   printf("In idex (exec.h)\n");
   if (e->decode){
     printf("here in decode(pc)\n");
