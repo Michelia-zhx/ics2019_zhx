2eba510cb0c74380fa56bdff8a28a1f885ff84b5
tracer-ics2019 <tracer@njuics.org>
2019-10-17 23:15:53 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     23:15:53 up 1 day, 3 min,  1 user,  load average: 1.42, 1.28, 1.44
    11a3a0bf69cb2f5e4bb7e8fa4a48e31619a662d0

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -78,7 +78,7 @@ static OpcodeEntry opcode_table [512] = {
   /* 0x48 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x4c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x50 */	EMPTY, IDEX(r, push), IDEX(r, push), IDEX(r, push),
-  /* 0x54 */	EMPTY, IDEX(r, push), EMPTY, IDEX(r, push),
+  /* 0x54 */	EMPTY, IDEX(r, push), IDEX(r, push), IDEX(r, push),
   /* 0x58 */	EMPTY, IDEX(r, pop), EMPTY, IDEX(r, pop),
   /* 0x5c */	EMPTY, IDEX(r, pop), IDEX(r, pop), IDEX(r, pop),
   /* 0x60 */	EMPTY, EMPTY, EMPTY, EMPTY,
