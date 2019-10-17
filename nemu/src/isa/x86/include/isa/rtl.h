a9e699d45e995f34401566cf7c2b7d11e010b967
tracer-ics2019 <tracer@njuics.org>
2019-10-17 09:28:01 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     09:28:01 up 10:15,  1 user,  load average: 2.21, 1.99, 1.77
    134073903e9f6e137b0cbf7ee0ce5d6999fb2ae9

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -26,8 +26,13 @@ static inline void rtl_sr(int r, const rtlreg_t* src1, int width) {
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
-  cpu.esp -= 4;
-  rtl_sm(&cpu.esp, src1, 4);
+  //cpu.esp -= 4;
+  //rtl_sm(&cpu.esp, src1, 4);
+  rtl_subi(&reg_l(R_ESP),&reg_l(R_ESP),4);
+  //printf("In rtl_push(), after sub, esp = %x\n",reg_l(R_ESP));
+  //pr(src1);
+  rtl_sm(&reg_l(R_ESP),src1,4);
+  return;
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
