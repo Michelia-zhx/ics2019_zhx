b375f80975f0b2bab61110db0e0bace7cd06953a
tracer-ics2019 <tracer@njuics.org>
2019-10-18 20:29:46 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     20:29:46 up 1 day, 21:17,  1 user,  load average: 2.69, 2.08, 1.90
    1b623293d608d411fbbecbe5f1cd876fb57579b7

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -67,7 +67,21 @@ make_EHelper(inc) {
 }
 
 make_EHelper(dec) {
-  TODO();
+  rtl_subi(&s0, &id_dest->val, 1);
+  rtl_setrelop(RELOP_LTU, &s1, &id_dest->val, &s0);
+
+  operand_write(id_dest, &s0);
+  
+  rtl_update_ZFSF(&s0, id_dest->val);
+  rtl_setrelop(RELOP_LTU, &t0, &id_dest->val, &s0);
+  rtl_or(&t0, &s1, &t0);
+  rtl_set_CF(&t0);
+  
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &s0);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template1(dec);
 }
