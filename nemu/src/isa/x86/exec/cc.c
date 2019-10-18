ec8c08f3ba1890cfd3a9bdd77b43680bf956d4ab
tracer-ics2019 <tracer@njuics.org>
2019-10-18 21:15:00 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     21:15:00 up 1 day, 22:02,  1 user,  load average: 3.04, 3.11, 2.99
    69f83f5bacf910492338ea4c52522a08ce9a0ca9

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -19,8 +19,8 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_E:  *dest = (cpu.eflags.ZF == 1 ? 1 : 0); break;
     case CC_BE:  *dest = (((cpu.eflags.CF | cpu.eflags.ZF) == 1) ? 1 : 0); break;
     case CC_S:  *dest = (cpu.eflags.SF == 1 ? 1 : 0); break;
-    case CC_L:  *dest = (((cpu.eflags.SF ^ cpu.eflags.OF) == 1) ? 1 : 0); break;
-    case CC_LE:  *dest = ((((cpu.eflags.SF ^ cpu.eflags.OF) | cpu.eflags.ZF) == 1) ? 1 : 0); break;
+    case CC_L:  *dest = (((cpu.eflags.SF != cpu.eflags.OF) == 1) ? 1 : 0); break;
+    case CC_LE:  *dest = (((cpu.eflags.SF != cpu.eflags.OF)==1 || (cpu.eflags.ZF == 1)) ? 1 : 0); break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
