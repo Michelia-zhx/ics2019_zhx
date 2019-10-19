27c1ed583b5edda7bf058fa1f894e8cdd52a2e45
tracer-ics2019 <tracer@njuics.org>
2019-10-19 15:42:40 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     15:42:40 up 2 days, 16:30,  1 user,  load average: 2.62, 2.31, 2.38
    752ae7d3c007673119e812f52a780d24a0ce9e

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -54,7 +54,9 @@ make_EHelper(shl) {
 }
 
 make_EHelper(shr) {
-  TODO();
+  rtl_shr(&id_dest->val, &id_dest->val, &id_src->val);
+  rtl_update_ZFSF(&id_dest->val, id_dest->width);
+  operand_write(id_dest, &id_dest->val);
   // unnecessary to update CF and OF in NEMU
 
   print_asm_template2(shr);
