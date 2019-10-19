82438978feec6574b31bfbce50cef79d8b2fa4e7
tracer-ics2019 <tracer@njuics.org>
2019-10-19 16:33:53 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     16:33:53 up 2 days, 17:21,  1 user,  load average: 3.31, 2.11, 1.90
    b0a7fb66ab5357c0e7fb7ffcba8ab74efc0a49fc

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -35,7 +35,7 @@ make_group(gp2,
 //Unary
 make_group(gp3,
     IDEX(test_I, test), EMPTY, EMPTY, EMPTY,
-    EX(mul), EX(mul), EX(mul), EX(idiv))
+    EMPTY, EX(mul), EX(mul), EX(idiv))
 
 /* 0xfe */
 //INC/DEC
