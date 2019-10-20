d978ea9456914415197a457ca7c249e4333730be
tracer-ics2019 <tracer@njuics.org>
2019-10-20 20:00:26 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     20:00:26 up 3 days, 20:48,  1 user,  load average: 3.16, 3.74, 3.10
    89a171d35eb68600e5f357be30277fa8233b7a1

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -102,8 +102,17 @@ make_EHelper(dec) {
 }
 
 make_EHelper(neg) {
-  TODO();
-
+  if (id_dest->val == 0) {
+    rtl_li(&s0, 0);
+    rtl_set_CF(&s0);
+  }
+  else {
+    rtl_li(&s0, 1);
+    rtl_set_CF(&s0);
+  }
+  rtl_li(&s0, 0);
+  rtl_sub(&id_dest->val, &s0, &id_dest->val);
+  operand_write(id_dest, &id_dest->val);
   print_asm_template1(neg);
 }
 
