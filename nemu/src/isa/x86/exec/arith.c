f274fc9daafde7663adc589b799ee3892cfcac87
tracer-ics2019 <tracer@njuics.org>
2019-10-22 21:06:10 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     21:06:10 up 5 days, 21:53,  1 user,  load average: 2.22, 2.22, 1.99
    ff7bf120c4861297800f10b093e9b623fcb3d1db

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -26,14 +26,11 @@ make_EHelper(sub) {
   rtl_sub(&s0, &id_dest->val, &id_src->val);
   if (id_dest->val < s0) s1 = 1;
   else s1 = 0;
-  //rtl_setrelop(RELOP_LTU, &s1, &id_dest->val, &s0);
-
   operand_write(id_dest, &s0);
 
   rtl_update_ZFSF(&s0, id_dest->width);
   if (id_dest->val < s0) t0 = 1;
   else t1 = 0;
-  //rtl_setrelop(RELOP_LTU, &t0, &id_dest->val, &s0);
   rtl_or(&t0, &s1, &t0);
   rtl_set_CF(&t0);
   
@@ -49,12 +46,13 @@ make_EHelper(sub) {
 make_EHelper(cmp) {
   //Log("%x %x",id_dest->val,id_src->val);
   rtl_sub(&s0, &id_dest->val, &id_src->val);
-  rtl_setrelop(RELOP_LTU, &s1, &id_dest->val, &s0);
+  if (id_dest->val < s0) s1 = 1;
+  else s1 = 0;
   
   rtl_update_ZFSF(&s0, id_dest->width);
   //Log("%x", cpu.eflags.ZF);
-  
-  rtl_setrelop(RELOP_LTU, &t0, &id_dest->val, &s0);
+  if (id_dest->val < s0) t0 = 1;
+  else t1 = 0;
   rtl_or(&t0, &s1, &t0);
   rtl_set_CF(&t0);
   
@@ -69,12 +67,16 @@ make_EHelper(cmp) {
 
 make_EHelper(inc) {
   rtl_addi(&s0, &id_dest->val, 1);
-  rtl_setrelop(RELOP_LTU, &s1, &s0, &id_dest->val);
+  if (s0 < id_dest->val) s1 = 1;
+  else s1 = 0;
+  //rtl_setrelop(RELOP_LTU, &s1, &s0, &id_dest->val);
 
   operand_write(id_dest, &s0);
 
   rtl_update_ZFSF(&s0, id_dest->width);
-  rtl_setrelop(RELOP_LTU, &t0, &s0, &id_dest->val);
+  if (s0 < id_dest->val) t0 = 1;
+  else t0 = 0;
+  //rtl_setrelop(RELOP_LTU, &t0, &s0, &id_dest->val);
   rtl_or(&t0, &s1, &t0);
   rtl_set_CF(&t0);
 
