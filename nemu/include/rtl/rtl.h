803074f910bb5b540103606678a8963ece97e1e7
tracer-ics2019 <tracer@njuics.org>
2019-10-22 20:32:56 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     20:32:56 up 5 days, 21:20,  1 user,  load average: 1.62, 1.50, 1.53
    28c840374d386a703551eaf04ff35f95af2cc732

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -138,7 +138,7 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   bool msb = (*src1 >> (width*8 - 1)) & 1;
-  if (!msb) *dest = *src1;
+  if (!msb) *dest = *src1 & 0x0000ffff;
   else {
     if (width == 4) *dest = *src1;
     else if (width == 2) *dest = (*src1 | 0xffff0000);
