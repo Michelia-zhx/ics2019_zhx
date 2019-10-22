eda0c431affd7bba6732781f04fb14c56dcb7220
tracer-ics2019 <tracer@njuics.org>
2019-10-22 20:01:29 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     20:01:29 up 5 days, 20:49,  1 user,  load average: 1.78, 1.72, 1.77
    298787a19af5b7d5cef83f7bf4e2d50f62af2f4d

--- nemu/src/isa/x86/exec/data-mov.c
+++ nemu/src/isa/x86/exec/data-mov.c
@@ -75,6 +75,7 @@ make_EHelper(movsx) {
   Log("%x", id_src->val);
   id_dest->width = decinfo.isa.is_operand_size_16 ? 2 : 4;
   s0=id_src->val;
+  Log("%x", s0);
   // rtl_sext(&s0, &id_src->val, id_src->width);
   operand_write(id_dest, &s0);
   Log("%x", id_dest->val);
