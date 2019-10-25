0dc6d36295670098a27ce04e3f5a9ad727c4b6b5
tracer-ics2019 <tracer@njuics.org>
2019-10-25 23:01:44 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     23:01:44 up 2 days, 14:00,  1 user,  load average: 2.01, 2.15, 2.00
    7f45abdd280828a1e15702d4500333a1f6fa287

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -8,10 +8,10 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
+  /*
   int ret = 0;
   int count, num;
   char *p;
-  printf("hello, 1");
   char numstr[100];
   while (*fmt){
     if (*fmt == '%'){
@@ -62,23 +62,98 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
     }
   }
   return 0;
+  */
+ int ret = 0;
+    int cnt,num;
+    char a[100];
+    char* p;
+    while(*fmt){
+        if(*fmt == '%'){
+            char op = *++fmt;
+            while('0' <= op && op <= '9') op = *++fmt;
+            switch(op){
+                case 's':
+                    p = va_arg(ap,char*);
+                    while(*p){
+                        *out++ = *p++;
+                        ret++;
+                    }
+                    break;
+                case 'd':
+                    num = va_arg(ap,int);
+                    cnt = 0;
+                    if(num < 0){
+                        *out++ = '-';
+                        num = -num;
+                    }
+                    if(!num) {
+                        *out++ = '0';
+                    }
+                    else
+                        {
+                        while(num)
+                            {
+                                a[++cnt] = num%10 + '0';
+                                num /= 10;
+                            }
+                        }
+
+                    while(num){
+                        a[++cnt] = num%10 + '0';
+                        num /= 10;
+                    }
+                    
+                    while(cnt){
+                        *out++ = a[cnt];
+                        cnt--;
+                        ret++;
+                    }
+                    break;
+                case 'x':
+                case 'p':
+                	num = va_arg(ap,int);
+                	char result[100];
+                	int step = 0; 
+					 do
+   					 {
+      					 result[step]="0123456789abcdef"[num%16];
+     					 num/=16;
+     					 step++;
+   					 }while(num);
+   					 result[step] = '\0';
+             		while(step){
+                        *out++ = result[step-1];
+                        step--;
+                        ret++;
+                    }
+             		break;
+                default:break;
+            }
+            fmt++;
+        }
+        else {
+            *out++ = *fmt++;
+            ret++;
+        }
+    }
+    *out = '\0';
+    return ret;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  /*
-  printf("hello, 2");
   int iReturn ;
   va_list pArgs ;
   va_start (pArgs, fmt) ;
   iReturn = vsprintf (out, fmt, pArgs) ;
   va_end (pArgs) ;
   return iReturn ;
-  */
+  /*
   va_list aptr;
   va_start(aptr, fmt);
   int ret = vsprintf(out, fmt, aptr);
   va_end(aptr);
   return ret;
+  */
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
