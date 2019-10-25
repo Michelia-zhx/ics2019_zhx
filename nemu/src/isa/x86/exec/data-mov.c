87260c791804196bbb50315a2435abe698f4ba73
tracer-ics2019 <tracer@njuics.org>
2019-10-25 19:44:59 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     19:44:59 up 2 days, 10:43,  1 user,  load average: 1.43, 1.42, 1.33
    9c59bdb87ea1bb28dab9c4588413a2f47d59a37f

--- nemu/src/isa/x86/exec/data-mov.c
+++ nemu/src/isa/x86/exec/data-mov.c
@@ -8,11 +8,9 @@ make_EHelper(mov) {
 make_EHelper(push) {
   //printf("here\n");
   rtl_push(&id_dest->val);
-  //rtlreg_t esp1 = cpu.esp;
   if (id_dest->type != OP_TYPE_IMM) operand_write(id_dest, &id_dest->val);
   //else if (id_dest->type == OP_TYPE_IMM) id_dest->imm = id_dest->val;
   //else assert(0);
-  //cpu.esp = esp1;
   
   print_asm_template1(push);
 }
--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,39 +3,131 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t length = 0;
+  char *p = s;
+  while(*p){
+    length += 1;
+    p =+ 1;
+  }
+  return length;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  char *p = dst;
+  while(*src){
+    *p = *src;
+    src += 1;
+    p += 1;
+  }
+  *p = '\0';
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  char *p = dst;
+  for (int i=1; i<=n; ++i){
+    if (*src){
+      *p = *src;
+      src += 1;
+      p += 1;
+    }
+    else{
+      *p = '\0';
+      p += 1;
+    }
+  }
+  if (*src) *p = '\0';
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  char *p = dst;
+  while (*p) p += 1;
+  while (*src){
+    *p = *src;
+    p += 1;
+    src += 1;
+  }
+  *p = '\0';
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  int result = 0;
+  while (*s1 && *s2 && (*s1 == *s2)){
+    s1 += 1;
+    s2 += 1;
+  }
+  if (!(*s1) && !(*s2)) result = 0;
+  else if (!(*s1) && *s2) result = -1;
+  else if (*s1 && !(*s2)) result = 1;
+  else if (*s1 < *s2) result = -1;
+  else if (*s1 > *s2) result = 1;
+  else {
+    printf("error in atrcmp");
+    assert(0);
+  }
+  return result;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  int result = 0;
+  char *p1 = s1;
+  char *p2 = s2;
+  n -=1;
+  while (*p1 && *p2 && n && (*p1==*p2)){
+      p1 += 1;
+      p2 += 1;
+      n -= 1;
+  }
+  if (!(*p1) && !(*p2)) result = 0;
+  else if (!(*p1) && *p2) result = -1;
+  else if (*p1 && !(*p2)) result = 1;
+  else if (*p1 > *p2) result = 1;
+  else if (*p1 < *p2) result = -1;
+  else result = 0;
+  return result;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  char *p = (char *)v;
+  for (int i=0; i < n; ++i) {
+    *p = c;
+    p += 1;
+  }
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  void *origin = out;
+  char *p = (char *)out;
+  char *q = (char *)in;
+  while (n > 0){
+    *p = *q;
+    p += 1;
+    q += 1;
+    n--;
+  }
+  return origin;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+  int result = 0;
+  char *p1 = (char *)s1;
+  char *p2 = (char *)s2;
+  n -=1;
+  while (*p1 && *p2 && n && (*p1==*p2)){
+      p1 += 1;
+      p2 += 1;
+      n -= 1;
+  }
+  if (!(*p1) && !(*p2)) result = 0;
+  else if (!(*p1) && *p2) result = -1;
+  else if (*p1 && !(*p2)) result = 1;
+  else if (*p1 > *p2) result = 1;
+  else if (*p1 < *p2) result = -1;
+  else result = 0;
+  return result;
 }
 
 #endif
