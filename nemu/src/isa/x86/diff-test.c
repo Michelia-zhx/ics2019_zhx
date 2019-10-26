68537aa2fbd09bb023ddfaef4137ef8bd575aa90
tracer-ics2019 <tracer@njuics.org>
2019-10-26 10:50:41 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     10:50:41 up 3 days,  1:49,  1 user,  load average: 1.65, 1.46, 1.44
    eff4db50a43f3b62b3340e9fae281f42f03ee4da

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -43,7 +43,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if ((*ref_r).gpr[3]._8[1] != cpu.gpr[3]._8[1]) return false;
   printf("6\n");
   
-  return false;
+  return true;
 }
 
 void isa_difftest_attach(void) {
