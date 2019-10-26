7897a1286b12fe2d852bc58a7563f95ffd6db073
tracer-ics2019 <tracer@njuics.org>
2019-10-26 10:35:29 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     10:35:29 up 3 days,  1:34,  1 user,  load average: 1.43, 1.48, 1.48
    a19ec12087087fc5cc8a7730fdf2e373f5819b29

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,7 +2,30 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-
+  if ((*ref_r).eax == cpu.eax) return false;
+  if ((*ref_r).ecx == cpu.ecx) return false;
+  if ((*ref_r).edx == cpu.edx) return false;
+  if ((*ref_r).ebx == cpu.ebx) return false;
+  if ((*ref_r).esp == cpu.esp) return false;
+  if ((*ref_r).ebp == cpu.ebp) return false;
+  if ((*ref_r).esi == cpu.esi) return false;
+  if ((*ref_r).edi == cpu.edi) return false;
+  if ((*ref_r).gpr[0]._16 == cpu.gpr[0]._16) return false;
+  if ((*ref_r).gpr[1]._16 == cpu.gpr[1]._16) return false;
+  if ((*ref_r).gpr[2]._16 == cpu.gpr[2]._16) return false;
+  if ((*ref_r).gpr[3]._16 == cpu.gpr[3]._16) return false;
+  if ((*ref_r).gpr[4]._16 == cpu.gpr[4]._16) return false;
+  if ((*ref_r).gpr[5]._16 == cpu.gpr[5]._16) return false;
+  if ((*ref_r).gpr[6]._16 == cpu.gpr[6]._16) return false;
+  if ((*ref_r).gpr[7]._16 == cpu.gpr[7]._16) return false;
+  if ((*ref_r).gpr[0]._8[0] == cpu.gpr[0]._8[0]) return false;
+  if ((*ref_r).gpr[0]._8[1] == cpu.gpr[0]._8[1]) return false;
+  if ((*ref_r).gpr[1]._8[0] == cpu.gpr[1]._8[0]) return false;
+  if ((*ref_r).gpr[1]._8[1] == cpu.gpr[1]._8[1]) return false;
+  if ((*ref_r).gpr[2]._8[0] == cpu.gpr[2]._8[0]) return false;
+  if ((*ref_r).gpr[2]._8[1] == cpu.gpr[2]._8[1]) return false;
+  if ((*ref_r).gpr[3]._8[0] == cpu.gpr[3]._8[0]) return false;
+  if ((*ref_r).gpr[3]._8[1] == cpu.gpr[3]._8[1]) return false;
   return true;
 }
 
