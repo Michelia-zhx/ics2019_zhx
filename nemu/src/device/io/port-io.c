40b6726f5d7cc0560c4390f84a399a2ed3e8d00e
tracer-ics2019 <tracer@njuics.org>
2019-10-30 22:35:49 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     22:35:49 up 4 days, 14 min,  1 user,  load average: 1.94, 1.78, 1.75
    8dcb52599b73619fbb663f69a5b93054f9bc5d19

--- nemu/src/device/io/port-io.c
+++ nemu/src/device/io/port-io.c
@@ -19,6 +19,7 @@ void add_pio_map(char *name, ioaddr_t addr, uint8_t *space, int len, io_callback
 }
 
 static inline uint32_t pio_read_common(ioaddr_t addr, int len) {
+  printf("here in pio_read_common\n");
   assert(addr + len - 1 < PORT_IO_SPACE_MAX);
   int mapid = find_mapid_by_addr(maps, nr_map, addr);
   assert(mapid != -1);
