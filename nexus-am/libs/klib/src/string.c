ff774cb73e204e567efe1a45834719cfe2ccb083
tracer-ics2019 <tracer@njuics.org>
2019-11-01 12:54:28 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     12:54:28 up 5 days, 14:33,  1 user,  load average: 1.70, 1.54, 1.62
    86c46242a61c844ba478831fce79ae08d8dee4ae

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,133 +3,114 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  size_t length = 0;
-  while(*s){
-    length += 1;
-    s += 1;
-  }
-  return length;
+  const char *p = s;
+  size_t len = 0;
+  for(; *p != '\0'; p++)
+    len++;
+  return len;
 }
 
 char *strcpy(char* dst,const char* src) {
-  char *p = dst;
-  while(*src){
-    *p = *src;
-    src += 1;
-    p += 1;
-  }
-  *p = '\0';
-  return dst;
+  return strncpy(dst, src, strlen(src));
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  char *p = dst;
-  for (int i=1; i<=n; ++i){
-    if (*src){
-      *p = *src;
-      src += 1;
-      p += 1;
-    }
-    else{
-      *p = '\0';
-      p += 1;
-    }
+  size_t len = strlen(src);
+  if(len < n)
+    return strcpy(dst, src);
+  else{
+    for(int i = 0; i < n; i++)
+      *(dst + i) = *(src + i);
   }
-  if (*src) *p = '\0';
   return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  char *p = dst;
-  while (*p) p += 1;
-  while (*src){
-    *p = *src;
-    p += 1;
-    src += 1;
-  }
-  *p = '\0';
-  return dst;
+  char *head = dst;
+  while(*dst != '\0')
+    dst++;
+  strcpy(dst, src);
+  return head;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  int result = 0;
-  while (*s1 && *s2 && (*s1 == *s2)){
-    s1 += 1;
-    s2 += 1;
+  while(*s1 == *s2){
+    s1++;
+    s2++;
+    if(*s1 == '\0' && *s2 == '\0')
+      return 0;
   }
-  if (!(*s1) && !(*s2)) result = 0;
-  else if (!(*s1) && *s2) result = -1;
-  else if (*s1 && !(*s2)) result = 1;
-  else if (*s1 < *s2) result = -1;
-  else if (*s1 > *s2) result = 1;
-  else {
-    printf("error in strcmp");
-    assert(0);
-  }
-  return result;
+  if(*s1 < *s2)
+    return -1;
+  else
+    return 1;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  /*
-  int result = 0;
-  char *p1 = s1;
-  char *p2 = s2;
-  n -=1;
-  while (*p1 && *p2 && n && (*p1==*p2)){
-      p1 += 1;
-      p2 += 1;
-      n -= 1;
+  size_t len = strlen(s2);
+  if(len <= n)
+    return strcmp(s1, s2);
+  else{
+    for(int i = 0; i < n;){
+      while(*s1 == *s2){
+        s1++;
+        s2++;
+        i++;
+        if(*s1 == '\0' || *s2 == '\0')
+          return 0; 
+      }
+      if(*s1 < *s2)
+        return -1;
+      else 
+        return 1;
+    }
   }
-  if (!(*p1) && !(*p2)) result = 0;
-  else if (!(*p1) && *p2) result = -1;
-  else if (*p1 && !(*p2)) result = 1;
-  else if (*p1 > *p2) result = 1;
-  else if (*p1 < *p2) result = -1;
-  else result = 0;
-  return result;
-  */
- return 0;
+  return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  char *p = (char *)v;
-  for (int i=0; i < n; ++i) {
-    *p = c;
-    p += 1;
+  char * v1;
+  v1 = (char*)v;
+  char c1 = '0' + c - 0;
+  for(int i = 0; i < n; i++){
+    *v1 = c1;
+    v1++;
   }
-  return v;
+  v1 = (void*)v1;
+  return v1;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  void *origin = out;
-  char *p = (char *)out;
-  char *q = (char *)in;
-  while (n > 0){
-    *p = *q;
-    p += 1;
-    q += 1;
-    n--;
+  char *out1;
+  const char *in1;
+  out1 = (char*)out;
+  in1 = (char*)in;
+  for(int i = 0; i < n; i++){
+    *out1 = *in1;
+    out1 ++;
+    in1 ++;
   }
-  return origin;
+  out1 = (void*)out1;
+  return out1;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  int result = 0;
-  char *p1 = (char *)s1;
-  char *p2 = (char *)s2;
-  n -=1;
-  while (*p1 && *p2 && n && (*p1==*p2)){
-      p1 += 1;
-      p2 += 1;
-      n -= 1;
-  }
-  if (!(*p1) && !(*p2)) result = 0;
-  else if (!(*p1) && *p2) result = -1;
-  else if (*p1 && !(*p2)) result = 1;
-  else if (*p1 > *p2) result = 1;
-  else if (*p1 < *p2) result = -1;
-  else result = 0;
-  return result;
+  /*
+  const char *s1_, *s2_;
+  s1_ = (char*)s1;
+  s2_ = (char*)s2;
+  for(int i = 0; i < n;){
+    while(*s1_ == *s2_){
+      s1_++;
+      s2_++;
+      i++;
+    }
+    if(*s1_ < *s2_)
+      return -1;
+    else 
+      return 1;
+  }*/
+  return 0; 
 }
 
 #endif
