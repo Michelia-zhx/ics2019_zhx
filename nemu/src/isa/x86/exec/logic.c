a476fad35320d7853e2a10156dee29262941b0bf
tracer-ics2019 <tracer@njuics.org>
2019-11-01 21:01:44 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     21:01:44 up 5 days, 22:40,  1 user,  load average: 1.75, 1.79, 1.89
    bc7b8f90dfa85df32cd3980e674929d9efef7044

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -100,3 +100,13 @@ make_EHelper(not) {
 
   print_asm_template1(not);
 }
+
+make_EHelper(rol){
+  
+  rtl_shl(&t0,&id_dest->val,&id_src->val);
+  rtl_shri(&t1,&id_dest->val,id_dest->width*8 - id_src->val);
+  rtl_or(&t0,&t0,&t1);
+  operand_write(id_dest,&t0);
+  
+  print_asm_template2(rol);
+}
