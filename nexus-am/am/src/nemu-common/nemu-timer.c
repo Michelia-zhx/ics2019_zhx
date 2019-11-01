541bcf66cf60b460c28530b145875cd74c615aab
tracer-ics2019 <tracer@njuics.org>
2019-11-01 18:51:47 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     18:51:47 up 5 days, 20:30,  1 user,  load average: 2.32, 1.96, 1.70
    9e82efe48ea2ca8d1affd31f2e69299521a8bc60

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -1,13 +1,13 @@
 #include <am.h>
 #include <amdev.h>
 #include <nemu.h>
-
+uint32_t start;
 size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
       uptime->hi = 0;
-      uptime->lo = 0;
+      uptime->lo = inl(0x48) - start;
       return sizeof(_DEV_TIMER_UPTIME_t);
     }
     case _DEVREG_TIMER_DATE: {
@@ -25,4 +25,5 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
 }
 
 void __am_timer_init() {
+  start = inl(0x48);
 }
