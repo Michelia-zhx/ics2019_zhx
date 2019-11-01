7af191e502037984f3f2a0fa7ef79a2deca21dad
tracer-ics2019 <tracer@njuics.org>
2019-11-01 13:03:28 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     13:03:28 up 5 days, 14:42,  1 user,  load average: 1.88, 1.78, 1.70
    a6cf2abc16f9902d6934e0543d6e5d45aec4b65f

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -4,78 +4,109 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
+  /*va_list ap;
+  int val, val1;
+  int re = 0;
+  char *s;
+  va_start(ap, fmt);
+  while(*fmt){
+    char s = *fmt;
+    if(strcmp(s, '%') == 0){
+      fmt++;
+      switch(fmt)
+        case'd':
+          val = va_arg(ap, int);
+          val1 = val;
+          int count = 0;
+          while(val1 != 0){
+            val1 /= 10;
+            count++;
+          }
+          re += count;
+          char v = atoi(val);
+          printf(v);
+          break;
+        case's':
+          s = va_arg(ap, char*);
+          re += strlen(s);
+          printf(s);
+          break;
+        default: return 0;break;
+     }
+    fmt++;
+  }
+  va_end(ap);
+  return re;
+  */
   return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  int ret = 0;
-  int count, num;
-  char *p;
-  char numstr[100];
-  while (*fmt){
-    if (*fmt == '%'){
-      fmt += 1;
-      char op = *fmt;
-      switch (op) {
-      case 'd':
-        num = va_arg(ap, int);
-        count = 0;
-        if (num < 0){
-          *out = '-';
-          out += 1;
-          num = 0-num;
-          while (num != 0){
-            numstr[count] = num % 10 + '0';
-            count += 1;
-            num /= 10;
-        }
-        }
-        else if (num == 0) *out = '0';
-        else {
-          while (num != 0){
-            numstr[count] = num % 10 + '0';
-            count += 1;
-            num /= 10;
+  int re = 0;
+  char *str;  
+  while(*fmt){ 
+    if(*fmt == '%'){
+      fmt++;
+      int val,val1;
+      switch(*fmt){
+        case 'd':
+          val = va_arg(ap, int);
+          val1 = val;
+          int count = 0;
+          while(val1){
+            val1 /= 10;
+            count ++;
           }
-        }
-        while (count != 0){
-          *out = numstr[count-1];
-          count -= 1;
-          ret += 1;
-          out += 1;
-        }
-        break;
-      case 's':
-        p = va_arg(ap, char*);
-        while (*p) {
-          *out = *p;
-          out += 1;
-          p += 1;
-          ret += 1;
-        }
-        break;
-     
-      default:
-        break;
-      }
-      fmt += 1;
+          val1 = val;
+          re += count;
+          char num[100];
+          int count1 = count;
+          while(count){
+            int t = val1 % 10;
+            num[count - 1] = t + '0';
+            val1 /= 10;
+            count--;
+          }
+          for(int i = 0; i < count1; i++){
+            *out = num[i];
+            //_putc(*out);
+            out++;
+          }
+          break;
+        case 's':
+          str = va_arg(ap, char *);
+          int len = strlen(str);
+          re += len; 
+          for(int i = 0; i < len; i++){
+            *out = *str;
+            //_putc(*out);
+            out++;
+            str++;
+          }
+          break;
+        default://printf("More to be added...\n"); 
+          break;
+        }  
+    fmt++;
     }
     else {
-            *out++ = *fmt++;
-            ret++;
+      *out = *fmt;
+      //_putc(*out);
+      out++;
+      fmt++;
+      re++;
     }
   }
   *out = '\0';
-  return 0;
+  return re;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  int iReturn ;
-  va_list ap ;
-  va_start (ap, fmt) ;
-  iReturn = vsprintf (out, fmt, ap) ;
-  va_end (ap) ;
-  return iReturn ;
+  va_list ap;
+  va_start(ap, fmt);
+  int re = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return re;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
