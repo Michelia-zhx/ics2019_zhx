d6bece7bcf4edbeaa6bc6f61e84c0e0bb9fe9fbf
tracer-ics2019 <tracer@njuics.org>
2019-11-01 15:45:55 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     15:45:55 up 5 days, 17:24,  1 user,  load average: 2.20, 2.05, 2.09
    68bb875559a49547e48ac767e3288bc86e25ecc

--- nexus-am/tests/amtest/src/tests/hello.c
+++ nexus-am/tests/amtest/src/tests/hello.c
@@ -5,7 +5,7 @@ static inline void putstr(const char *s) {
 }
 
 void hello() {
-  for (int i = 0; i < 6; i ++) {
+  for (int i = 0; i < 7; i ++) {
     putstr("Hello, AM World @ " __ISA__ "\n");
   }
 }
