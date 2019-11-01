9ecaba3fe8a3f435d8894f77ac7bf950294cd95e
tracer-ics2019 <tracer@njuics.org>
2019-11-01 14:30:23 +0800

    >  compile 181220064 skywalkerx Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:30:23 up 5 days, 16:09,  1 user,  load average: 2.51, 2.49, 2.27 acf418db72d294b58f3c37a436d7bd0239cf97f

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -14,26 +14,47 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:  
-      *dest = (cpu.eflags.OF == 1 ? 1 : 0);
+    case CC_O:
+      if(cpu.eflags.OF == 1)
+        *dest = 1;
+      else
+        *dest = 0;
       break;
     case CC_B:
-      *dest = (cpu.eflags.CF == 1 ? 1 : 0);
+      if(cpu.eflags.CF == 1)
+        *dest = 1;
+      else
+        *dest = 0;
       break;
     case CC_E:
-      *dest = (cpu.eflags.ZF == 1 ? 1 : 0);
+      if(cpu.eflags.ZF == 1)
+        *dest = 1;
+      else
+        *dest = 0;
       break;
     case CC_BE:
-      *dest = ((cpu.eflags.ZF | cpu.eflags.CF) == 1 ? 1 : 0);
+      if(cpu.eflags.CF == 1 || cpu.eflags.ZF == 1)
+        *dest = 1;
+      else
+        *dest = 0;
       break;
     case CC_S:
-      *dest = (cpu.eflags.SF == 1 ? 1 : 0);
+      if(cpu.eflags.SF == 1)
+        *dest = 1;
+      else
+        *dest = 0;
       break;
     case CC_L:
-      *dest = ((cpu.eflags.SF ^ cpu.eflags.OF) == 1 ? 1 : 0);
+      if(cpu.eflags.SF != cpu.eflags.OF)
+        *dest = 1;
+      else
+        *dest = 0;
       break;
     case CC_LE:
-      *dest = (((cpu.eflags.ZF | (cpu.eflags.SF ^ cpu.eflags.OF)) == 1) ? 1 : 0);
+      if(cpu.eflags.SF != cpu.eflags.OF || cpu.eflags.ZF == 1)
+        *dest = 1;
+      else
+        *dest = 0;
       break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -2,78 +2,67 @@
 #include "cc.h"
 
 make_EHelper(test) {
-  rtl_and(&id_dest->val, &id_dest->val, &id_src->val);
-  rtl_update_ZFSF(&id_dest->val, id_dest->width);
-  rtl_li(&s0, 0);
-  rtl_set_CF(&s0);
-  rtl_set_OF(&s0);
-  
+  rtl_and(&t1, &id_dest->val, &id_src->val);
+  rtl_update_ZFSF(&t1, id_dest->width);
+  t0 = 0;
+  rtl_set_CF(&t0);
+  rtl_set_OF(&t0);
   print_asm_template2(test);
 }
 
 make_EHelper(and) {
-  rtl_and(&id_dest->val, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &id_dest->val);
-  rtl_li(&s0, 0);
-  rtl_set_CF(&s0);
-  rtl_set_OF(&s0);
-  rtl_update_ZFSF(&id_dest->val, id_dest->width);
-
+  rtl_and(&t1, &id_dest->val, &id_src->val);
+  rtl_update_ZFSF(&t1, id_dest->width);
+  operand_write(id_dest,&t1);
+  t0 = 0; 
+  rtl_set_CF(&t0);
+  rtl_set_OF(&t0);
   print_asm_template2(and);
 }
 
 make_EHelper(xor) {
-  rtl_xor(&id_dest->val, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &id_dest->val);
-  rtl_li(&s0, 0);
-  rtl_set_CF(&s0);
-  rtl_set_OF(&s0);
+  rtl_xor(&s0,&id_dest->val, &id_src->val);
+  operand_write(id_dest,&s0);
   rtl_update_ZFSF(&id_dest->val, id_dest->width);
 
+  t0 = 0;
+  rtl_set_CF(&t0);
+  rtl_set_OF(&t0);
   print_asm_template2(xor);
 }
 
 make_EHelper(or) {
-  rtl_or(&id_dest->val, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &id_dest->val);
-  rtl_li(&s0, 0);
-  rtl_set_CF(&s0);
-  rtl_set_OF(&s0);
+  rtl_or(&s0,&id_dest->val, &id_src->val);
+  operand_write(id_dest,&s0);
   rtl_update_ZFSF(&id_dest->val, id_dest->width);
+
+  t0 = 0;
+  rtl_set_CF(&t0);
+  rtl_set_OF(&t0);
 
   print_asm_template2(or);
 }
 
 make_EHelper(sar) {
-  /*
-  if (id_dest->width == 1){
-    id_dest->val = (int8_t)id_dest->val;
-  }
-  else if (id_dest->width == 2){
-    id_dest->val = (int16_t)id_dest->val;
-  }
-  */
-  rtl_sar(&id_dest->val, &id_dest->val, &id_src->val);
-  rtl_update_ZFSF(&id_dest->val, id_dest->width);
-  operand_write(id_dest, &id_dest->val);
+  rtl_sar(&s0, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &s0);
+  rtl_update_ZFSF(&id_dest->val, id_dest->width);//rtl_update_ZFSF(&s0, id_dest->width);
   // unnecessary to update CF and OF in NEMU
-
   print_asm_template2(sar);
 }
 
 make_EHelper(shl) {
-  rtl_shl(&id_dest->val, &id_dest->val, &id_src->val);
-  rtl_update_ZFSF(&id_dest->val, id_dest->width);
-  operand_write(id_dest, &id_dest->val);
   // unnecessary to update CF and OF in NEMU
-
+  rtl_shl(&s0, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &s0);
+  rtl_update_ZFSF(&id_dest->val, id_dest->width);//rtl_update_ZFSF(&s0, id_dest->width);
   print_asm_template2(shl);
 }
 
 make_EHelper(shr) {
-  rtl_shr(&id_dest->val, &id_dest->val, &id_src->val);
-  rtl_update_ZFSF(&id_dest->val, id_dest->width);
-  operand_write(id_dest, &id_dest->val);
+  rtl_shr(&s0, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &s0);
+  rtl_update_ZFSF(&id_dest->val, id_dest->width);//如果有错就是这里
   // unnecessary to update CF and OF in NEMU
 
   print_asm_template2(shr);
@@ -83,13 +72,7 @@ make_EHelper(setcc) {
   uint32_t cc = decinfo.opcode & 0xf;
 
   rtl_setcc(&s0, cc);
-  // Log("%x",s0);
-  // Log("%x",cpu.eax);
-  if (s0)
-  {
-    operand_write(id_dest, &s0);
-  }
-  // Log("%x",cpu.eax);
+  operand_write(id_dest, &s0);
 
   print_asm("set%s %s", get_cc_name(cc), id_dest->str);
 }
@@ -97,6 +80,5 @@ make_EHelper(setcc) {
 make_EHelper(not) {
   rtl_not(&id_dest->val, &id_dest->val);
   operand_write(id_dest, &id_dest->val);
-
   print_asm_template1(not);
 }
