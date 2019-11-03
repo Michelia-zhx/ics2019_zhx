a0309a99889e296673ddd84a7fcecaa995358e0b
tracer-ics2019 <tracer@njuics.org>
2019-11-03 11:59:34 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-65-generic #74-Ubuntu SMP Tue Sep 17 17:06:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     11:59:34 up 7 days, 13:38,  1 user,  load average: 1.89, 1.84, 1.72
    d15f62dfb1d15a75bdd2afdb2b29839499f7ba7e

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -14,7 +14,7 @@ bool interpret_relop(uint32_t relop, const rtlreg_t src1, const rtlreg_t src2);
 /* RTL basic instructions */
 
 //static inline void interpret_rtl_li(rtlreg_t* dest, uint32_t imm) {
-void interpret_rtl_li(rtlreg_t* dest, uint32_t imm) {
+inline void interpret_rtl_li(rtlreg_t* dest, uint32_t imm) {
   printf("hi\n");
   *dest = imm;
 }
