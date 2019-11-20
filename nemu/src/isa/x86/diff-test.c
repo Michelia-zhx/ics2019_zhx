60240b6142577853d9121f2bf6a1294aca74446f
tracer-ics2019 <tracer@njuics.org>
2019-11-20 19:18:14 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-66-generic #75-Ubuntu SMP Tue Oct 1 05:24:09 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     19:18:14 up 3 days, 23:09,  1 user,  load average: 1.87, 1.93, 1.76
    f2bacdbc365391a4d7a8d7137313129ed18ddc6b

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -3,6 +3,13 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   printf("ref_r.eax: %x, cpu.eax: %x\n", (*ref_r).eax, cpu.eax);
+  printf("ref_r.ecx: %x, cpu.ecx: %x\n", (*ref_r).ecx, cpu.ecx);
+  printf("ref_r.edx: %x, cpu.edx: %x\n", (*ref_r).edx, cpu.edx);
+  printf("ref_r.ebx: %x, cpu.ebx: %x\n", (*ref_r).ebx, cpu.ebx);
+  printf("ref_r.esp: %x, cpu.esp: %x\n", (*ref_r).esp, cpu.esp);
+  printf("ref_r.ebp: %x, cpu.ebp: %x\n", (*ref_r).ebp, cpu.ebp);
+  printf("ref_r.esi: %x, cpu.esi: %x\n", (*ref_r).esi, cpu.esi);
+  printf("ref_r.edi: %x, cpu.edi: %x\n", (*ref_r).edi, cpu.edi);
   if (ref_r->pc != cpu.pc) return false;
   for (int i=0; i<8; ++i){
     if (ref_r->gpr[i]._32 != cpu.gpr[i]._32) return false;
