a6307aa7de1258d40ecc978b1b1166835bd1ca72
tracer-ics2019 <tracer@njuics.org>
2019-11-20 19:31:42 +0800

    >  compile
    skywalkerx
    Linux zhx-book 4.15.0-66-generic #75-Ubuntu SMP Tue Oct 1 05:24:09 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     19:31:42 up 3 days, 23:22,  1 user,  load average: 1.60, 1.60, 1.66
    d74a3c8eb6b66f14237bce7d4d2c6c87679aa91

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -10,35 +10,19 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   printf("ref_r.ebp: %x, cpu.ebp: %x\n", (*ref_r).ebp, cpu.ebp);
   printf("ref_r.esi: %x, cpu.esi: %x\n", (*ref_r).esi, cpu.esi);
   printf("ref_r.edi: %x, cpu.edi: %x\n", (*ref_r).edi, cpu.edi);
+  /*
   if (ref_r->pc != cpu.pc) return false;
   for (int i=0; i<8; ++i){
     if (ref_r->gpr[i]._32 != cpu.gpr[i]._32) return false;
   }
-  /*
-  if ((*ref_r).eax != cpu.eax) return false;
-  if ((*ref_r).ecx != cpu.ecx) return false;
-  if ((*ref_r).edx != cpu.edx) return false;
-  if ((*ref_r).ebx != cpu.ebx) return false;
-  if ((*ref_r).esp != cpu.esp) return false;
-  if ((*ref_r).esi != cpu.esi) return false;
-  if ((*ref_r).edi != cpu.edi) return false;
-  if ((*ref_r).gpr[0]._16 != cpu.gpr[0]._16) return false;
-  if ((*ref_r).gpr[1]._16 != cpu.gpr[1]._16) return false;
-  if ((*ref_r).gpr[2]._16 != cpu.gpr[2]._16) return false;
-  if ((*ref_r).gpr[3]._16 != cpu.gpr[3]._16) return false;
-  if ((*ref_r).gpr[4]._16 != cpu.gpr[4]._16) return false;
-  if ((*ref_r).gpr[5]._16 != cpu.gpr[5]._16) return false;
-  if ((*ref_r).gpr[6]._16 != cpu.gpr[6]._16) return false;
-  if ((*ref_r).gpr[7]._16 != cpu.gpr[7]._16) return false;
-  if ((*ref_r).gpr[0]._8[0] != cpu.gpr[0]._8[0]) return false;
-  if ((*ref_r).gpr[0]._8[1] != cpu.gpr[0]._8[1]) return false;
-  if ((*ref_r).gpr[1]._8[0] != cpu.gpr[1]._8[0]) return false;
-  if ((*ref_r).gpr[1]._8[1] != cpu.gpr[1]._8[1]) return false;
-  if ((*ref_r).gpr[2]._8[0] != cpu.gpr[2]._8[0]) return false;
-  if ((*ref_r).gpr[2]._8[1] != cpu.gpr[2]._8[1]) return false;
-  if ((*ref_r).gpr[3]._8[0] != cpu.gpr[3]._8[0]) return false;
-  if ((*ref_r).gpr[3]._8[1] != cpu.gpr[3]._8[1]) return false;
   */
+  if (ref_r->eax != cpu.eax) return false;
+  if (ref_r->ecx != cpu.ecx) return false;
+  if (ref_r->edx != cpu.edx) return false;
+  if (ref_r->ebx != cpu.ebx) return false;
+  if (ref_r->esp != cpu.esp) return false;
+  if (ref_r->esi != cpu.esi) return false;
+  if (ref_r->edi != cpu.edi) return false;
   return true;
 }
 
