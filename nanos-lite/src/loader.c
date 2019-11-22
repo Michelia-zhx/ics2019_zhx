2bd9050ab54f801adf223b1d5f99dea296a42de8
tracer-ics2019 <tracer@njuics.org>
2019-11-22 20:03:04 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-66-generic #75-Ubuntu SMP Tue Oct 1 05:24:09 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     20:03:04 up 20:32,  1 user,  load average: 2.08, 1.71, 1.67
    e40a970413e51d7152710da4a6fce039a0999e6e

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -51,7 +51,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     printf("p_memsize: %d\n", phdr.p_memsz);
     switch (phdr.p_type) {
       case PT_LOAD: {
-        // ramdisk_write((void *)(phdr.p_vaddr), phdr.p_offset, phdr.p_filesz);
+        ramdisk_write((void *)(phdr.p_vaddr), phdr.p_offset, phdr.p_filesz);
         memset((void*)(phdr.p_vaddr+phdr.p_filesz), 0, phdr.p_memsz-phdr.p_filesz);
         printf("hello\n");
         break;
