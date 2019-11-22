a4779ccb46c2df5807c22218efb392ab4a86004c
tracer-ics2019 <tracer@njuics.org>
2019-11-22 14:47:01 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-66-generic #75-Ubuntu SMP Tue Oct 1 05:24:09 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     14:47:01 up 15:15,  1 user,  load average: 2.07, 2.20, 2.10
    904efc4dd3a8092fc16033fb341325482882ddd3

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -5,9 +5,15 @@
 #ifdef __ISA_AM_NATIVE__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
+# define Elf_off Elf64_Off
+# define Elf_Half Elf64_Half
+# define Elf_Word Elf64_Word
 #else
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
+# define Elf_off Elf32_Off
+# define Elf_Half Elf32_Half
+# define Elf_Word Elf32_Word
 #endif
 
 size_t get_ramdisk_size();
@@ -21,7 +27,21 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr ehdr;
   size_t len = ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
   printf("len %d\n", len);
-  printf("ehdr.type:%d\n", ehdr.e_type);
+  Elf_off phdr_offset = ehdr.e_phoff;
+  // Elf_off shdr_offset = ehdr.e_shoff;
+  Elf_Half phdr_size = ehdr.e_phentsize;
+  // Elf_Half shdr_size = ehdr.e_shentsize;
+  Elf_Half phdr_num = ehdr.e_phnum;
+  // Elf_Half shdr_num = ehdr.e_shnum;
+  Elf_Phdr phdr;
+  for (int i=0; i<phdr_num; ++i) {
+    len = ramdisk_read(&phdr, phdr_offset+i*phdr_size, phdr_size);
+    if (phdr.p_type != PT_LOAD) continue;
+    Elf_off p_offset = phdr.p_offset;
+    Elf_Word p_filesize = phdr.p_filesz;
+    Elf_Word p_memsize = phdr.p_memsz;
+    printf("i: %d, p_offset: %d, p_filesize: %d, p_memsize: %d\n", i, p_offset, p_filesize, p_memsize);
+  }
   return 0;
 }
 
