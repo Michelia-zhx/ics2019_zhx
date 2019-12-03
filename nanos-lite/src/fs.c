b2c717ed450ae659d22412454bf63ba035205165
tracer-ics2019 <tracer@njuics.org>
2019-12-03 20:54:24 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     20:54:24 up 1 day, 21 min,  1 user,  load average: 1.87, 1.94, 1.95
    e568d12546f7501673dad9935cf52fc1f54cba28

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -73,6 +73,7 @@ size_t fs_read(int fd, void *buf, size_t len){
     printf("file_table[%d].read == NULL\n", fd);
     // size_t read_len = len;
     printf("len: %d, file_table[%d].size: %d, file_table[fd].read_offset: %d.\n", len, fd, file_table[fd].size, file_table[fd].read_offset);
+    printf("file_table[%d].disk_offset: %d.\n", fd, file_table[fd].disk_offset);
     // if (file_table[fd].read_offset + len > file_table[fd].size)
       // read_len = file_table[fd].size - file_table[fd].read_offset;
     ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].read_offset, len);
