019139c4f0cc3186e71e4b10253c6dfb48ca4510
tracer-ics2019 <tracer@njuics.org>
2019-12-03 21:20:06 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     21:20:06 up 1 day, 47 min,  1 user,  load average: 3.15, 2.32, 2.05
    d727f32923e800e046c6a4c940d9062ae25821

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -89,7 +89,7 @@ size_t fs_read(int fd, void *buf, size_t len){
 
 size_t fs_write(int fd, const void *buf, size_t len){
   size_t write_len;
-  if (file_table[fd].write == NULL)
+  if (file_table[fd].write != NULL)
     write_len = file_table[fd].write(buf, file_table[fd].disk_offset+file_table[fd].read_offset, len);
   else
     write_len = ramdisk_write(buf, file_table[fd].disk_offset+file_table[fd].read_offset, len);
