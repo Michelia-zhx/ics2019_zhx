4005fff59a10e4049fb88d6f3546aa434b9f5f4d
tracer-ics2019 <tracer@njuics.org>
2019-12-03 20:18:32 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     20:18:32 up 23:45,  1 user,  load average: 2.04, 2.64, 2.57
    a4da09438fe3961ed00f53b6ee734e0a98dbf403

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -72,6 +72,7 @@ size_t fs_read(int fd, void *buf, size_t len){
   if (file_table[fd].read == NULL){
     printf("file_table[%d].read == NULL\n", fd);
     size_t read_len = len;
+    printf("len: %d.\n", len);
     if (file_table[fd].read_offset + len > file_table[fd].size)
       read_len = file_table[fd].size - file_table[fd].read_offset;
     printf("have computed read_len = %d\n", read_len);
