3df54ff426c4d521a0d638542251a49c8a5b66f7
tracer-ics2019 <tracer@njuics.org>
2019-12-03 21:05:52 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     21:05:52 up 1 day, 33 min,  1 user,  load average: 1.91, 1.82, 1.89
    1318ec0190ffbab8f1ed70e53a0b47c1ad6cda3

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void hello_fun(void *arg) {
 void init_proc() {
   switch_boot_pcb();
   // Log("hello");
-  naive_uload(NULL, "/bin/text");
+  naive_uload(NULL, NULL);
 
   Log("Initializing processes...");
 
