30b17a6f8a041991f9aeba00cc2ccba74abca782
tracer-ics2019 <tracer@njuics.org>
2019-12-03 21:24:31 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     21:24:31 up 1 day, 51 min,  1 user,  load average: 2.16, 2.36, 2.14
    470377e6f8fb9b9dae737a0fc869df31d39e19d

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -23,7 +23,7 @@ _Context* do_syscall(_Context *c) {
 
     case SYS_write:
     // Log("SYS_write");
-    /*
+    
       if (a[1]==1 || a[1]==2){  // a[1] = fd
         char *addr = (char *)(a[2]);  // a[2] = (intptr_t)buf
         uintptr_t count = a[3];  // a[3] = count
@@ -34,8 +34,8 @@ _Context* do_syscall(_Context *c) {
         }
       }
       c->GPRx = a[3]; // On success, the number of bytes written  is  returned
-      */
-      c->GPRx = fs_write(a[1], (void *)a[2], a[3]);
+      
+      // c->GPRx = fs_write(a[1], (void *)a[2], a[3]);
       break;
 
     case SYS_brk:
