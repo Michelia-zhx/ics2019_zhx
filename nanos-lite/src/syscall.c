ad6111d560cb81ee0afcd9be40ba575bbed297c4
tracer-ics2019 <tracer@njuics.org>
2019-12-04 17:29:51 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     17:29:51 up 1 day, 20:57,  1 user,  load average: 2.36, 1.78, 1.61
    43fac42cfb9c05b31e31a0c41950124d57f5682

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,7 +11,7 @@ _Context* do_syscall(_Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-  printf("a[0]: %d", a[0]);
+  printf("a[0]: %d\n", a[0]);
 
   switch (a[0]) {
     case SYS_yield:
