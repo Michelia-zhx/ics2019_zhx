1d85eba29b9027fb8011caaff7647c9b08fc1c41
tracer-ics2019 <tracer@njuics.org>
2019-12-04 18:27:28 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     18:27:28 up 1 day, 21:54,  1 user,  load average: 1.53, 1.75, 1.66
    dcbde5c855349e7a37d50f5c7a75122db7f46562

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -54,7 +54,7 @@ size_t fbsync_write(const void *buf, size_t offset, size_t len) {
 void init_device() {
   Log("Initializing devices...");
   _ioe_init();
-
+  
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
   sprintf(dispinfo,"WIDTH: %d, HEIGHT: %d\n",screen_width(),screen_height());
