3480bbbf1cb35cdeca7fe40ba3c0c74099c0de2d
tracer-ics2019 <tracer@njuics.org>
2019-12-05 19:38:41 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     19:38:41 up 2 days, 23:06,  1 user,  load average: 1.71, 1.52, 1.48
    97e9e6d9985ad1d0a2b5d7bcebc2295aefb758d

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,14 +22,15 @@ static const char *keyname[256] __attribute__((used)) = {
 size_t events_read(void *buf, size_t offset, size_t len) {
   // printf("in events_read\n");
   int key = read_key();
-  int down = 0;
-  if (key & 0x8000) down = 1;
-  else down = 0;
+  // int down = 0;
+  // if (key & 0x8000) down = 1;
+  // else down = 0;
   key = key & 0x7fff;
   if (key != _KEY_NONE) {
-    // Log("has key");
-    if (down) sprintf(buf, "down1:\n");
-    else sprintf(buf, "down0:\n");
+    int time = uptime();
+    sprintf(buf, "t %d\n", time);
+    // if (down) sprintf(buf, "down1:\n");
+    // else sprintf(buf, "down0:\n");
   }
   else {
     int time = uptime();
