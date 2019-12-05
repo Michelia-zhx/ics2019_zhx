5fe9cf6754ae21a821b6765400e5d2045e0e323d
tracer-ics2019 <tracer@njuics.org>
2019-12-05 22:38:24 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     22:38:24 up  1:34,  1 user,  load average: 1.20, 1.30, 1.22
    2aa228abd5d494ddb90f9e8fbd7ac3648d1c77c7

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -3,14 +3,16 @@
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
-
+unsigned long long press;
 size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
-      int code = inl(0x60);
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      kbd->keydown = ((code >> 15) & 1);
-      kbd->keycode = code & 0x7fff;
+      press = inl(KBD_ADDR);
+      kbd->keycode = press;
+      if (press != _KEY_NONE){
+        kbd->keydown = !(kbd->keydown);
+      }
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }
