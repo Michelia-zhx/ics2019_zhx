c7ab78e5a74075959ddbb197bfba5025f607d33f
tracer-ics2019 <tracer@njuics.org>
2019-12-06 23:06:26 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     23:06:26 up 1 day,  2:02,  1 user,  load average: 1.61, 1.91, 2.40
    3a0d806b1944e4d2210cb0554bf9897b613a1d

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -101,12 +101,13 @@ size_t fs_read(int fd, void *buf, size_t len){
   }
   ramdisk_read(buf,file_table[fd].disk_offset + file_table[fd].open_offset,len);
   file_table[fd].open_offset+=len;
-  // Log("%d\n",file(fd).open_offset);
+  // Log("%d\n",file_table[fd].open_offset);
   return len;
   */
 }
 
 size_t fs_write(int fd, const void *buf, size_t len){
+  /*
   if (fd==1 || fd==2){
     char *addr = (char *)(buf);
     uintptr_t count = len;
@@ -130,6 +131,19 @@ size_t fs_write(int fd, const void *buf, size_t len){
     file_table[fd].open_offset += write_len;
     return write_len;
   }
+  */
+  if (file_table[fd].size > 0 && file_table[fd].open_offset+len > file_table[fd].size) {
+    len = file_table[fd].size - file_table[fd].open_offset;
+  }
+  if (file_table[fd].write!=NULL) {
+    size_t ret = file_table[fd].write(buf,file_table[fd].open_offset,len);
+    file_table[fd].open_offset+=ret;
+    return ret;
+  }
+  // Log("%s\n",(char*)buf);
+  ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
+  file_table[fd].open_offset+=len;
+  return len;
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence){
