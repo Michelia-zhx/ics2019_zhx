9e1dd48cc038d21ece552107af49a905d2d2adf3
tracer-ics2019 <tracer@njuics.org>
2019-12-06 22:41:38 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     22:41:38 up 1 day,  1:37,  1 user,  load average: 3.40, 3.74, 3.25
    1aa2f1e2a9e50117cf5b349e7872379529eb0f4a

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -39,11 +39,11 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, 0, invalid_read, invalid_write},
   {"stdout", 0, 0, 0, invalid_read, serial_write},
   {"stderr", 0, 0, 0, invalid_read, serial_write},
-  {"/dev/events", 0, 0, 0, events_read, invalid_write},
+  {"/dev/events", 0, 0, 0, events_read, NULL},
   {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
-  {"/dev/fdsync", 0, 0, 0, invalid_read, fbsync_write},
-  {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
-  {"/dev/tty", 0, 0, 0, invalid_read, serial_write},
+  {"/dev/fdsync", 0, 0, 0, NULL, fbsync_write},
+  {"/proc/dispinfo", 30, 0, 0, dispinfo_read, NULL},
+  {"/dev/tty", 0, 0, 0, NULL, serial_write},
 #include "files.h"
 };
 
