096a4b23fe449d427974c3f18b1db73d94b6d74b
tracer-ics2019 <tracer@njuics.org>
2019-12-07 00:22:43 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     00:22:43 up 1 day,  3:18,  1 user,  load average: 2.19, 1.82, 1.73
    b6f97b8ddf4e106686021295d83a1f70f6e49ac6

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -46,7 +46,7 @@ static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   memcpy(buf, dispinfo + offset, len);
-  return strlen(buf);
+  return len;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
