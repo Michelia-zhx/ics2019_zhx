469f56a31f098a6a516da177fbb9f636f49265b0
tracer-ics2019 <tracer@njuics.org>
2019-12-07 00:02:32 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     00:02:32 up 1 day,  2:58,  1 user,  load average: 1.48, 1.52, 1.54
    65bc8fb67b3e8d61e68a617324e98e2478997c5

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -4,12 +4,19 @@
 
 int main() {
   NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
+  printf("1\n");
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
+  printf("2\n");
   assert(bmp->pixels);
+  printf("3\n");
   NDL_OpenDisplay(bmp->w, bmp->h);
+  printf("4\n");
   NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
+  printf("5\n");
   NDL_Render();
+  printf("6\n");
   NDL_CloseDisplay();
-  while (1);
+  printf("7\n");
+ // while (1);
   return 0;
 }
