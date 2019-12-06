b718d84d3c0c65366aebac25405f04ced3d72423
tracer-ics2019 <tracer@njuics.org>
2019-12-06 22:50:26 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     22:50:26 up 1 day,  1:46,  1 user,  load average: 2.52, 3.16, 3.21
    d29772bcd466479a27ca909d53343fc77a2c1df1

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -94,9 +94,11 @@ size_t fs_read(int fd, void *buf, size_t len){
     return file_table[fd].read(buf, file_table[fd].open_offset-len, len);
   }
   */
+ /*
   if (file_table[fd].read == events_read && file_table[fd].open_offset == file_table[fd].size){
     file_table[fd].open_offset = 0;
   }
+  */
   if (file_table[fd].open_offset + len > file_table[fd].size) {
     len = file_table[fd].size - file_table[fd].open_offset;
   }
