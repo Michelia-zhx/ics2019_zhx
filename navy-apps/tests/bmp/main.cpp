76f4fd793f73c2755741d0b308ce184127c06dcd
tracer-ics2019 <tracer@njuics.org>
2019-12-07 00:26:12 +0800

    >  run
    skywalkerx
    Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     00:26:12 up 1 day,  3:21,  1 user,  load average: 1.90, 1.94, 1.81
    794d76eb967bd21294943306849a45accb82a0

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -1,23 +1,15 @@
 #include <assert.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <ndl.h>
 
 int main() {
   NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
-  printf("1\n");
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
-  printf("2\n");
   assert(bmp->pixels);
-  printf("3\n");
   NDL_OpenDisplay(bmp->w, bmp->h);
-  printf("4\n");
   NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
-  printf("5\n");
   NDL_Render();
-  printf("6\n");
   NDL_CloseDisplay();
-  printf("7\n");
- // while (1);
+  while (1);
   return 0;
 }
