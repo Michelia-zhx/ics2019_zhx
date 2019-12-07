76ff47138ef67dcd89fbcf2d8b4cc27021c56f4b
tracer-ics2019 <tracer@njuics.org>
2019-12-07 17:08:19 +0800

    >  run 181220005 skywalkerx Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:08:19 up 1 day, 20:03,  1 user,  load average: 2.17, 2.11, 2.14 8436e1eccc97a42db82c08e7bfa6eda7c9412b3a

--- nemu/src/isa/x86/decode/decode.c
+++ nemu/src/isa/x86/decode/decode.c
@@ -31,17 +31,9 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
+  op->simm = instr_fetch(pc, op->width);
+  if(op->width == 1) op->simm = (op->simm & 0x80 ? op->simm | 0xffffff00 : op->simm);
   
-  if(op->width==4){
-		uint32_t temp = instr_fetch(pc, op->width);
-		int32_t result = temp;
-		op->simm = result;
-	}
-  else{
-		uint8_t temp = instr_fetch(pc, op->width);
-		int8_t result = temp;
-		op->simm = result;
-  }
   rtl_li(&op->val, op->simm);
 
   print_Dop(op->str, OP_STR_SIZE, "$0x%x", op->simm);
