7d6658b9e46676f8cda324372631f2380681f7c3
tracer-ics2019 <tracer@njuics.org>
2019-12-07 17:52:47 +0800

    >  compile 181220005 skywalkerx Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:52:47 up 1 day, 20:48,  1 user,  load average: 2.58, 2.54, 2.45 3d34c5c82b1c55c39115b143698cf8a3561bfabf

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -125,11 +125,21 @@ make_EHelper(not) {
 }
 
 make_EHelper(rol){
-  
+/*  
   rtl_shl(&t0,&id_dest->val,&id_src->val);
   rtl_shri(&t1,&id_dest->val,id_dest->width*8 - id_src->val);
   rtl_or(&t0,&t0,&t1);
   operand_write(id_dest,&t0);
   
   print_asm_template2(rol);
+*/
+  rtl_li(&s0,id_dest->val);
+  for (int i=0; i<id_src->val; i++) {
+    rtl_msb(&s1,&s0,id_dest->width);
+    rtl_shli(&s0,&s0,1);
+    rtl_add(&s0,&s0,&s1);
+  }
+  operand_write(id_dest,&s0);
+
+  print_asm_template1(rol);
 }
