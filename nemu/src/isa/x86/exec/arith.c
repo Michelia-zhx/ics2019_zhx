853653a88ac228abb3760a156dbf800ac266fff8
tracer-ics2019 <tracer@njuics.org>
2019-12-07 17:41:08 +0800

    >  compile 181220005 skywalkerx Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:41:08 up 1 day, 20:36,  1 user,  load average: 2.40, 2.52, 2.31 59ca389f8289df9926229daed0286df6aaada29

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -43,6 +43,7 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
+/*
   // s0 = dest - src
   rtl_sub(&s0, &id_dest->val, &id_src->val);
   if (id_dest->val < s0) s1 = 1;
@@ -61,6 +62,24 @@ make_EHelper(sub) {
   rtl_msb(&t0, &t0, id_dest->width);
   rtl_set_OF(&t0);
 
+  print_asm_template2(sub);
+*/
+  rtl_sub(&s0, &id_dest->val, &id_src->val);
+  
+  operand_write(id_dest, &s0);
+ 
+  if (id_dest->width != 4) {
+    rtl_andi(&s0, &s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
+  }
+
+  rtl_update_ZFSF(&s0, id_dest->width);
+
+  rtl_is_sub_carry(&s1, &s0, &id_dest->val);
+  rtl_set_CF(&s1);
+
+  rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
+  rtl_set_OF(&s1);
+  
   print_asm_template2(sub);
 }
 
