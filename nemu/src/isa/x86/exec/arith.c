60b1655cf8a71b5d55ae24b117df39979cef09a8
tracer-ics2019 <tracer@njuics.org>
2019-12-07 18:01:41 +0800

    >  compile 181220005 skywalkerx Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:01:41 up 1 day, 20:57,  1 user,  load average: 2.52, 2.37, 2.36 3ffc7d474cdb57a6dbbd9ea8403064261b2b897d

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -1,6 +1,7 @@
 #include "cpu/exec.h"
 
 make_EHelper(add) {
+/*
   rtl_add(&s0, &id_dest->val, &id_src->val);
   
   operand_write(id_dest, &s0);
@@ -16,6 +17,27 @@ make_EHelper(add) {
 
   rtl_is_add_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
   rtl_set_OF(&s1);
+
+  print_asm_template2(add);
+*/
+  rtl_add(&s0, &id_dest->val, &id_src->val);
+  if (s0 < id_dest->val) s1 = 1;
+  else s1 = 0;
+
+  operand_write(id_dest, &s0);
+
+  rtl_update_ZFSF(&s0, id_dest->width);
+  if (s0 < id_dest->val) t0 = 1;
+  else t0 = 0;
+  rtl_or(&t0, &s1, &t0);
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_not(&t0, &t0);
+  rtl_xor(&t1, &id_dest->val, &s0);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template2(add);
 }
