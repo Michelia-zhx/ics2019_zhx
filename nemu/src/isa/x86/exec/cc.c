84f9bdba5b26c32b5c0aa5e8a9b6c6319c89561d
tracer-ics2019 <tracer@njuics.org>
2019-12-07 17:38:12 +0800

    >  run 181220005 skywalkerx Linux zhx-book 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:38:12 up 1 day, 20:33,  1 user,  load average: 3.01, 2.61, 2.29 573cf7f27c15ee6edc7067dfabfc2023909ae6e1

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -14,13 +14,27 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:  rtl_get_OF(&s0); *dest=s0?1:0; break;
-    case CC_B:  rtl_get_CF(&s0); *dest=s0?1:0; break;
-    case CC_E:  rtl_get_ZF(&s0); *dest=s0?1:0; break;
-    case CC_BE: rtl_get_ZF(&s0); rtl_get_CF(&s1); *dest=(s0||s1)?1:0; break;
-    case CC_S:  rtl_get_SF(&s0); *dest=s0?1:0; break;
-    case CC_L:  rtl_get_SF(&s0); rtl_get_OF(&s1); *dest=(s0!=s1)?1:0; break;
-    case CC_LE: rtl_get_SF(&s0); rtl_get_OF(&s1); rtl_get_ZF(&t1); *dest=(t1||(s0!=s1))?1:0; break;
+    case CC_O:  
+      *dest = (cpu.eflags.OF == 1 ? 1 : 0);
+      break;
+    case CC_B:
+      *dest = (cpu.eflags.CF == 1 ? 1 : 0);
+      break;
+    case CC_E:
+      *dest = (cpu.eflags.ZF == 1 ? 1 : 0);
+      break;
+    case CC_BE:
+      *dest = ((cpu.eflags.ZF | cpu.eflags.CF) == 1 ? 1 : 0);
+      break;
+    case CC_S:
+      *dest = (cpu.eflags.SF == 1 ? 1 : 0);
+      break;
+    case CC_L:
+      *dest = ((cpu.eflags.SF ^ cpu.eflags.OF) == 1 ? 1 : 0);
+      break;
+    case CC_LE:
+      *dest = (((cpu.eflags.ZF | (cpu.eflags.SF ^ cpu.eflags.OF)) == 1) ? 1 : 0);
+      break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
